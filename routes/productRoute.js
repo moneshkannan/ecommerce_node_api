dc1053755f44ff72a1e894b0bd33f298ad7521ce
moneshkannan <moneshkannan02@gmail.com>
2024-03-20 22:50:36 +0530

    added wishlist to product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct, addToWishlist } = require('../controllers/productController');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin,createProduct)
 router.get("/", getAllProduct)
+router.put("/wishlist", authMiddleware, addToWishlist)
 router.get("/:id", getProduct)
 router.put("/:id", authMiddleware, isAdmin, updateProduct)
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct)
+
+
 module.exports = router
